8d50972dbb2af7f1be69c85f7b75cf2edae569c7
latikadh04-hub <latika.dh04@gmail.com>
2025-10-07 08:07:56 +0530

    Even and Odd program

--- Latika/program.py
+++ Latika/program.py
@@ -1,6 +1,11 @@
 print("Even and ODD program")
-num = 10
-if num%2 == 0 :
-    print("It is Even")
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Please enter a valid integer.")
+    exit(1)
+
+if num % 2 == 0:
+    print(f"{num} is Even")
 else:
-    print("It is Odd")
+    print(f"{num} is Odd")
