e9dd71a06245d12679d49eda2e6a56e4801bf677
maha26994 <mahalakshmi26694@gmail.com>
2025-10-08 07:47:45 +0530

    python variable

---
+++ Maha/PythonBasics/python_variable.py
@@ -0,0 +1,23 @@
+a = 10
+b = 20
+print (a)
+print (b)
+
+c = a + b
+
+print (c)
+print ("The value of c is ",c)
+
+###Assign multiple variables at a time###
+
+x, y, z = 50, 60, 70
+print ("The value of x:", x)
+print ("The value of y:", y)
+print ("The value of z:", z)
+
+###Assign single value to mutliple variable
+
+p = q = r =100
+print ("P is ", p)
+print ("q is ", q)
+print ("r is ", r)
