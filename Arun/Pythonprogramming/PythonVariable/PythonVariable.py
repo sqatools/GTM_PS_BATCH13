eacad78ba3dd09d96074415fc84dfab9f04b9bf8
Aruntej786 <aruntej786@gmail.com>
2025-10-08 07:59:43 +0530

    first python program to git edit code for python

---
+++ Arun/Pythonprogramming/PythonVariable/PythonVariable.py
@@ -0,0 +1,8 @@
+x, y, z = 50, 60, 70
+print("value of x:", x)  # value of x : 50
+print("value of y:", y)  # value of y : 60
+print("value of z:", z)  # value of z : 70
+a = 10
+b = 20
+print(a)
+print('b')
