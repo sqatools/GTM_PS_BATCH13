65daebd78af7ee7c413ab25263cd67673e11ae34
sangeetasalian <sangeeta.salian@gmail.com>
2025-10-09 22:41:59 -0400

    pushing data types to Git

---
+++ Sangeeta/PythonVariable/datatype.py
@@ -0,0 +1,21 @@
+print("Numbers data tpes - Integer, Float, Complex")
+print("Integer / float data types")
+a = 458
+b = 985.4589654
+c = 78.5j
+print("Value of a:", a, type(a))
+print("Value of b:", b, type(b))
+print("Value of c:", c, type(c))
+print(" ")
+
+print("Sequential data types")
+st1 = 'Sequential'
+st2 = "Datatype"
+st3 = '''Python is a high-level, interpreted, object-oriented programming language known for its clear syntax
+ and readability. 
+ Created by Guido van Rossum and first released in 1991, its design philosophy emphasizes code readability, 
+ making it popular for both beginners and experienced developers. 
+'''
+print("Value of st1", st1, type(st1))
+print("Value of st2:", st2, type(st2))
+print("value of st3:",st3, type(st3))
