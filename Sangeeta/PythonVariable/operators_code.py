b9af6ad748bbfea72350acc6447b1aecf1c3da91
sangeetasalian <sangeeta.salian@gmail.com>
2025-10-09 21:22:31 -0400

    Pyushing first 5 python problems as HW to Git

--- Sangeeta/PythonVariable/operators_code.py
+++ Sangeeta/PythonVariable/operators_code.py
@@ -14,44 +14,57 @@ print ("17*2=", a2)
 print ("17*3=", a3)
 print ("17*4=", a4)
 print ("17*5=", a5)
+print(" ")
 
 print("Division")
 n1 = 1278451
 n2 = 78954
+print("n1=", n1)
+print("n2 =", n2)
 c = n1//n2
 d = n1%n2
-
 print("Value of n1/n2=", c)
 print("Remainder of n1/n2=", d)
-
+print(" ")
 print ("Addition")
-x=500*500
-y=45*90
-z=x+y
+x = 500*500
+y = 45*90
+z = x+y
+print(x)
+print(y)
 print("Value of x+y=", z)
-
-print ("Subtraction")
+print(" ")
+print("Subtraction")
 l1 = (a4+a5)
 l2 = 10
 l3 = l1-l2
-
+print(l1)
+print(l2)
 print ("Value of l3=", l3)
+print(" ")
 
 print("Compare Values")
 abc = 'Name77'
 xyz = '77Name'
 a2z = 'Name77'
+print(abc)
+print(xyz)
+print(a2z)
 
 print(abc == xyz)
 print(abc == a2z)
 print (xyz == a2z)
+print(" ")
 
 print("Quadratic Equation")
 a=5
 b=6
 lhs = (a+b)**2
 rhs = a**2+b**2+2*a*b
-print (lhs == rhs)
+print(lhs)
+print(rhs)
+print(lhs == rhs)
+
 
 
 
