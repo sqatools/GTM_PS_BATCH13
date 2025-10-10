852c4b4330e8f77e83b79c4dd4545200fd725f89
Nacchu123 <guravpreeti2022@example.com>
2025-10-10 08:09:50 +0530

    classwork 9 oct

--- Preeti/Py_Program/Python_Variable/classwork_9october.py
+++ Preeti/Py_Program/Python_Variable/classwork_9october.py
@@ -1,49 +1,49 @@
-#Python Program to add two integer values.
+#1Python Program to add two integer values.
 
 x1=int(20)
 x2=int(50)
 print("Adding 2 integer values:",x1+x2)
 
-#Python Program to subtract two integer values.
+#2Python Program to subtract two integer values.
 
 x1=30
 x3=40
 print("Substracting 2 integer values:",x1-x2)
 
-#Python program to multiply two numbers.
+#3Python program to multiply two numbers.
 
 x1=80
 x2=80
 print("Multipliplying 2 integer values",x1*x2)
 
-#Python program to repeat a given string 5 times.
+#4Python program to repeat a given string 5 times.
 
-str1 = 'SQATools'
+str1 ='SQATools'
 n=5
 print(str1*n)
 
-#Python program to get the Average of given numbers.
+#5Python program to get the Average of given numbers.
 a=40
 b=50
 c=30
 n=("Average of number:", (a+b+c)/3)
 print(n)
 
-#Python program to get the median of given numbers.
+#6Python program to get the median of given numbers.
 
 
 
 
-#Python program to print the square and cube of a given number.
+#7Python program to print the square and cube of a given number.
 
 print("square of 9:",9**2)
 print("cube of 9:",9*9*9)
 
-#Python program to interchange values between variables.
+#8Python program to interchange values between variables.
 a=10
 b=20
 a,b=b,a
 print("value of a:",a)
 print("value of b:",b)
 
-#Python program to solve this Pythagorous theorem.
+#9Python program to solve this Pythagorous theorem.
