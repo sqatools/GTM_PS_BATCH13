4be9e17a2f8647e687f38daca226a9f34f41e514
shabe2304 <shabel2304@gmail.com>
2025-10-10 08:32:14 +0530

    New Learing   - Data Types

--- Shalaka/Basic_Python/DataTypes.py
+++ Shalaka/Basic_Python/DataTypes.py
@@ -11,9 +11,9 @@ num1 = 0
 num2 = 23
 num3 = 45678909876554678
 
-print ('num1 :', type(num1))
-print ('num2 :', type(num2))
-print ('num3 :', type(num3))
+print ('num1 :',num1, type(num1))
+print ('num2 :',num2, type(num2))
+print ('num3 :',num3, type(num3))
 
 print('_'*20)
 print('Data Types - 1.Number - Float')
@@ -24,9 +24,9 @@ f1 = 0.0
 f2 = 23.30
 f3 = 156567.787889
 
-print ('f1 :', type(f1))
-print ('f2 :', type(f2))
-print ('f3 :', type(f3))
+print ('f1 :',f1, type(f1))
+print ('f2 :',f2, type(f2))
+print ('f3 :',f3, type(f3))
 
 print('_'*20)
 print('Data Types - 1.Number - Complex')
@@ -38,23 +38,35 @@ comp2 = 60j+50
 comp3 = 10*20j
 comp4 = 60j*50
 
-print ('comp1 :', type(comp1))
-print ('comp2 :', type(comp2))
-print ('comp3 :', type(comp3))
-print ('comp4 :', type(comp4))
+print ('comp1 :',comp1, type(comp1))
+print ('comp2 :',comp2, type(comp2))
+print ('comp3 :',comp3, type(comp3))
+print ('comp4 :',comp4, type(comp4))
 
-print('_'*20)
+
 print('_'*20)
 
 print('Sequential - String')
 
 s1 = ''
 s2 = "Shalaka"
-s3 = """ Hello world !!!"""
+s3 = """ Let the improvement of yourself keep you so busy that you have no time to criticize others."""
 
-print ('s1 :', type(s1))
-print ('s2 :', type(s2))
-print ('s3 :', type(s3))
+print ('s1 :', s1, type(s1))
+print ('s2 :', s2, type(s2))
+print ('s3 :', s3, type(s3))
+
+print('_'*20)
+print('Sequential - Positive & Negative String')
+
+char = "All the best"
+
+print(char[0])
+print(char[-4])
+
+
+
+
 
 
 
