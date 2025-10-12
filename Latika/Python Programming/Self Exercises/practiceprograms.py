122cb0694f9e8b0accb9efad08d2505a8c39d8a0
latikadh04-hub <latika.dh04@gmail.com>
2025-10-12 15:53:55 +0530

    Self Practice

--- Latika/Python Programming/Self Exercises/practiceprograms.py
+++ Latika/Python Programming/Self Exercises/practiceprograms.py
@@ -12,9 +12,8 @@ else:
 print("The Median Value is :",{median_value})
 
 ######################################################################
-
+print("Right Triangle Star Pattern in Python")
 rows = 5  # Number of rows
-
 for i in range(1, rows + 1):  # Loop for rows
     for j in range(i):  # Loop for columns (stars)
         print("*", end=" ")  # Print star
@@ -22,10 +21,15 @@ for i in range(1, rows + 1):  # Loop for rows
 
 
 ##################################################################
-
-
+print("Inverted Right-Angled Triangle Star Pattern in Python")
 number=5
 for i in range(number,0, - 1):
     for j in range(i):
         print("*",end=" ")
     print()
+###############################################################
+
+print("Maximum value from tuple")
+tup=[40,50,30,90]
+print("Maximum value is :",max(tup))
+print("Minimum value is:",min(tup))
