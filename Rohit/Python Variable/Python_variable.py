1e5edb1b06daaad6f1d574ccaa93d4e4475a5344
rohitgit0006 <rohitchavan0006@gmail.com>
2025-10-13 18:03:56 +0530

    Adding python file to git.

---
+++ Rohit/Python Variable/Python_variable.py
@@ -0,0 +1,28 @@
+a=25
+b=40
+c=a+b
+print(c)
+print("Value of c:" ,c)
+
+#############Assign Multiple value to multiple variable at a time###########
+x,y,z=15,33,42
+print(x,y,z)
+print("value of x:",x)
+print("value of y:",y)
+print("value of z:",z)
+
+print("value of y:",y)
+print("value of z:",z)
+print("value of x:",x)
+
+################Assign Single value to multiple variables################
+p=q=r=35
+print(p,q,r)
+
+print("value of p:",p)
+print("value of q:",q)
+print("value of r:",r)
+
+print("p:",p)
+print("q:",q)
+print("r:",r)
