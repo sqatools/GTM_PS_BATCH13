402bc4af4f6fffdb941957908dde413a7dc314b3
maxpartha88 <max.partha@gmail.com>
2025-10-14 01:38:43 -0400

    Adding Homework v2 from 13th Oct File in Partha

--- Partha/Homework_101325.py
+++ Partha/Homework_101325.py
@@ -20,3 +20,16 @@ b = 53
 c = 74
 print("The Average of", a, b, "and", c, "is", (a+b+c)/3)
 print("*"*50)
+
+nlist1 = [10,45,33,76,23,108,92]
+slist1 = sorted(nlist1)
+x=len(slist1)
+print(slist1)
+print("Median of nlist1 is",slist1[x//2])
+print("*"*50)
+nlist2 = [46,104,45,77,19,93]
+slist2 = sorted(nlist2)
+y=len(slist2)
+print(slist2)
+print("Median of nlist2 is",(slist2[(y//2)-1]+slist2[y//2])/2)
+print("*"*50)
