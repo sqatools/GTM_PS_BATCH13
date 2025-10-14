d55b3cafcaf3b2b942f14028c97aede42a765014
maxpartha88 <max.partha@gmail.com>
2025-10-14 00:28:43 -0400

    Adding Homework from 13th Oct File in Partha

---
+++ Partha/Homework_101325.py
@@ -0,0 +1,22 @@
+a = 1
+b = 2
+print("1.","Sum of a & b is",a+b)
+# from where the space is coming after "is" in the print statement?
+print("*"*50)
+
+print("Subtraction of a from b is",b-a)
+print("Subtraction of b from a is",a-b)
+print("*"*50)
+
+print("Multiplication of a & b is",a*b)
+print("*"*50)
+
+string = "Hello Player 1 "
+print(string*5)
+print("*"*50)
+
+a = 33
+b = 53
+c = 74
+print("The Average of", a, b, "and", c, "is", (a+b+c)/3)
+print("*"*50)
