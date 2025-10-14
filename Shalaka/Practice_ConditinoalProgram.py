9c431ac639965938fca8ef15ce3966c6810265e9
shabe2304 <shabel2304@gmail.com>
2025-10-14 20:40:39 +0530

    New Learing- Conditional Statements Assignment

--- Shalaka/Practice_ConditinoalProgram.py
+++ Shalaka/Practice_ConditinoalProgram.py
@@ -5,10 +5,32 @@ if num1%3 == 0:
 else:
     print("number is not divied by 3:", num1)
 
-print('_'*5, "2). If else program to get all the numbers divided by 3 from 1 to 30.", '_'*5)
-
 
 print('_'*5, "3). If else program to assign grades as per total marks.", '_'*5)
+
+marks = 102
+print(marks)
+
+if marks < 40:
+    print('fail')
+elif marks>=40 and marks <= 50:
+    print('Grade C')
+elif marks>=51 and marks <= 60:
+    print('Grade B')
+elif marks>=61 and marks <= 70:
+    print('Grade A')
+elif marks>=71 and marks <= 80:
+    print('Grade A+')
+elif marks>=81 and marks <= 90:
+    print('Grade A++')
+elif marks>=91 and marks <= 100:
+    print('Excellent')
+else:
+    print('Invalid Marks')
+
+
+
+
 
 print('_'*5, "4). Python program to check the given number divided by 3 and 5.", '_'*5)
 
@@ -32,6 +54,18 @@ if 55%11 == 0:
 
 else:
     print('no is not div by 11:', num3)
+
+print('_'*5, "7). Python program to check given number is odd or even.", '_'*5)
+
+number1 = 55
+if number1%2 == 0:
+    print('Given number is even:', number1)
+else :
+    print('Given number is odd:', number1 )
+
+
+
+
 
 
 
