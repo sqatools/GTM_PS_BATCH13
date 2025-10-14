472da272efb9a04f8a223c07c9c41c9b1511a0e9
Annuj-Sawant <annujksawant@gmail.com>
2025-10-14 07:50:40 +0530

    python variables code

--- Anuj/Python Programming/python_variable.py
+++ Anuj/Python Programming/python_variable.py
@@ -12,3 +12,20 @@ p=q=r=500
 print("p:",p)
 print("q:",q)
 print("r:",r)
+
+#### if variable value is same then their address is also same
+A=50
+B=60
+C=50
+print("value of A:",A,id(A))
+print("value of B:",B,id(B))
+print("value of C:",C,id(C))
+
+
+####Rules to Declare Variables######
+
+#1 There should not be space in variable name
+#2 Cannot start with a number
+#3 No limit for variable name length
+#4 Variable Name cannot contain special character except underscore(_)
+#5 Variable Names are case sensitive
