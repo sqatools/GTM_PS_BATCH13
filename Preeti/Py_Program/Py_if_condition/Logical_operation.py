362d025850135dfefb50764ec820a59b5f13a328
Nacchu123 <guravpreeti2022@example.com>
2025-10-15 08:19:26 +0530

    15 octo class work

--- Preeti/Py_Program/Py_if_condition/Logical_operation.py
+++ Preeti/Py_Program/Py_if_condition/Logical_operation.py
@@ -11,3 +11,21 @@ elif c>a and c>b:
     print("C is largest")
 else:
     print("No one is largest")
+
+############ in operator #########
+
+list=[2,3,5,7]
+n1=10
+if n1 in list:
+    print("n1 is there in list")
+else:
+    print("n1 is not there in list")
+
+########## not in operator ############
+
+str1="Hello world"
+val="JAVA"
+if val not in str1:
+    print("val is not available in the string")
+else:
+    print("val is available in the string")
