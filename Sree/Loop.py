a58d8d88f05593483a6bbf8c99bb30b20677ea0b
SreeKarri <jyokarri@gmail.com>
2025-10-16 23:01:55 -0400

    First file

--- Sree/Loop.py
+++ Sree/Loop.py
@@ -123,6 +123,15 @@ for i in range(7,1,-1):
     for j in range(1, i-1):
         print("*",end=" ")
     print()
+####################################
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end= " ")
+    print()
+
+
+
 
 
 
