8ce32a0e6480aeacdf10cf033f8cd8491385e31e
swarna <tripathy.swarna7@gmail.com>
2025-10-22 21:34:33 +0100

    for loop

---
+++ swarna/for_loop_tst.py
@@ -0,0 +1,13 @@
+
+
+
+# There's another kind of loop — the `for` loop.
+
+# It looks like this:
+
+for sita in ["a", "b", "c"]:
+  print(f"This letter is {sita}")
+
+for rama in range(0, 10):
+    print(f"This number is {rama}")
+
