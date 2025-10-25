87d3b303a03526562d36471e497de7714afbf096
maxpartha88 <max.partha@gmail.com>
2025-10-25 16:54:44 -0400

    Adding updated Classwork file for 1024

--- Partha/Classwork_102425.py
+++ Partha/Classwork_102425.py
@@ -1,15 +1,15 @@
-import re
-
-str8 = "It's my Life. I live by my own rules. 12345y test."
-word_list = str8.split(" ")
+str8 = "It's my Life. I live by my own rule. It's a test."
+translator = str.maketrans('','','.')
+cleanstr = str8.translate(translator)
+word_list = cleanstr.split(" ")
 print(word_list)
-max_word =''
+max_word = []
 max_length = 0
 for word in word_list:
     word_length = len(word)
     if word_length>max_length:
         max_length = word_length
 for word in word_list:
-    if len(word) == max_length:
-        max_word = max_word+" "+word+", "
+    if len(word) == max_length and word not in max_word:
+        max_word.append(word)
 print("The List of words with maximum length", max_length, "characters are: ", max_word)
