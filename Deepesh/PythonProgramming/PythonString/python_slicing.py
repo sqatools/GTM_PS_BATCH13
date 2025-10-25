1df34bb53e84237e9295c14fcacc4b40d42b806d
Deepesh <xovek11875@bitvoo.com>
2025-10-25 08:31:46 +0530

    Python String Practice

---
+++ Deepesh/PythonProgramming/PythonString/python_slicing.py
@@ -0,0 +1,24 @@
+
+
+str1 = "Hello"
+"""
+ 0  1  2  3  4    +ve
+ H  e  l  l  o
+-5 -4 -3 -2 -1   -ve
+"""
+
+print(str1[1])  # e
+print(str1[-4]) # e
+
+str2 = "Programming"
+print(str2[5])  # a
+print(str2[-8]) # g
+
+
+str3 = "Python Programming"
+# str[start:end:step]
+print(str3[0:6:1])  # Python
+print(str3[0:10:2]) # Pto r
+print(str3[7:18:1]) # Programming
+
+
