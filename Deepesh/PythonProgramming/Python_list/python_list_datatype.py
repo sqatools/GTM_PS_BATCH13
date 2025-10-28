ed8fe3e5b43c7b073da626bfc6c028e674bda3ce
Deepesh <xovek11875@bitvoo.com>
2025-10-28 07:54:07 +0530

    Python list data type

--- Deepesh/PythonProgramming/Python_list/python_list_datatype.py
+++ Deepesh/PythonProgramming/Python_list/python_list_datatype.py
@@ -128,3 +128,172 @@ print("l8 :", l8) # [7, 8, 9, 10, 20, 30]
 # dict1 = {'a': 123}
 # dict1['b'] = 456
 # print(dict1)
+
+print("_"*50)
+##########################################################
+# remove method :  remove any specific value from list
+
+list_b = [6, 7, 9, 2, 45]
+list_b.remove(9)
+print("list_b :", list_b)  # list_b : [6, 7, 2, 45]
+
+print("_"*50)
+##########################################################
+# pop method :  this method remove value from list using index position.
+#              ->  default index position is -1
+#              -> This method return the removed value, that we can store in a variable
+
+list_c=  [5, 7, 2, 15, 18]
+
+# remove default index -1
+v1 = list_c.pop()
+print("removed value :", v1)  # 18
+print("list_c :", list_c)  # [5, 7, 2, 15]
+
+# remove from specific index
+v2 = list_c.pop(1)
+print("removed value :", v2) # 7
+print("list_c :", list_c) # [5, 2, 15]
+
+
+print("_"*50)
+##########################################################
+# clear method : clear all data from list
+
+list1 = [5, 7, 9, 2]
+list1.clear()
+print("list1 :", list1) # list1 : []
+
+print("_"*50)
+############################################################
+# copy() method:
+
+# Shallow copy: when we assign one list to another list, and modify any value in both the list
+# then changes will reflect in both lists.
+
+list_d = [4, 7, 9, 23]
+list_e = list_d
+list_e.append(100)
+list_d.append(200)
+
+print("list_e :", list_e)
+print("list_d :", list_d)
+
+
+print("_"*50)
+# Deep Copy: In this concept we have to copy method to create a of list, and if we will do modification in any of the list
+# then changes will not reflect in another list.
+
+list_x = ['a', 'b', 'c']
+list_y = list_x.copy()
+list_x.append('d')
+list_y.append(100)
+print("list_x :", list_x) # ['a', 'b', 'c', 'd']
+print("list_y :", list_y) # ['a', 'b', 'c', 100]
+
+print("_"*50)
+#####################################################
+# sort method :
+list1 = [5, 8, 1, 2, 6, 100]
+# sort in ascending order
+list1.sort()
+print("list1 :", list1)  # [1, 2, 5, 6, 8, 100]
+
+# sort in descending order
+list2 = [5, 8, 15, 1, 2, 6, 100, 20]
+list2.sort(reverse=True)
+print("list2 :", list2)  # [100, 20, 15, 8, 6, 5, 2, 1]
+
+print("_"*50)
+#####################################################
+# reverse method : this method reverse entire list
+
+list_x = [5, 7, 9, 23, 4, 78, 'Hello', 'Python']
+list_x.reverse()
+print("list x :", list_x)
+# ['Python', 'Hello', 78, 4, 23, 9, 7, 5]
+
+print("_"*50)
+####################################################
+# write a python program to get all the even values from list
+list1 = [4, 7, 9, 2, 8, 12, 11]
+output = []
+
+for val in list1:
+    if val%2 == 0:
+        output.append(val)
+    else:
+        continue
+
+print("output :", output)  # [4, 2, 8, 12]
+
+# solve above program to with list comprehension
+result = [abc for abc in list1 if abc%2 == 0]
+print("Result :", result)
+
+
+
+print("_"*50)
+####################################################
+# write a python program to get all the required output values from list.
+
+list2 = [4, 7, 9, 12]
+#output = [(4, 'even'), (7, 'odd'), (9, 'odd'), (12, 'even')]
+output = []
+for val in list2:
+    if val%2 == 0:
+        output.append((val, 'even'))
+    else:
+        output.append((val, 'odd'))
+
+
+print("output :", output) # [(4, 'even'), (7, 'odd'), (9, 'odd'), (12, 'even')]
+
+
+
+# solve above program with list comp.
+list2 = [4, 7, 9, 12]
+result2 = [(y, "even") if y%2 == 0 else (y, "odd") for y in list2]
+print("result2 :", result2)
+
+result3 = ["even" if y%2 == 0 else "odd" for y in list2]
+print("result3 :", result3)
+
+result4 = [y**2 if y%2 == 0 else y**3 for y in list2]
+print("result4 :", result4) # [16, 343, 729, 144]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Deep Copy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
