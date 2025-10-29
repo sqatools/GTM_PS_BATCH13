6a4fbdb0d726431c6282b506e9f097b9d527994f
Dwijeshb <dwijeshbadari@gmail.com>
2025-10-29 07:47:25 +0530

    push

--- dwijesh/Python programing/python_list/list.py
+++ dwijesh/Python programing/python_list/list.py
@@ -38,3 +38,63 @@ print("output :", output)  # [4, 2, 8, 12]
 result = [abc for abc in list1 if abc%2 == 0]
 print("Result :", result)
 
+#####################################################
+
+list_a=[1,2,3,4,5,6,7,8,9]
+print(max(list_a))
+print(min(list_a))
+print(sum(list_a))
+
+#code without using max inbuilt function
+x=0
+for i in list_a:
+    if x>i:
+        x=i
+    else:
+        continue
+
+print(i)
+
+#code to remove duplicate vales in list
+list_dup=[1,2,3,2,3,4,1,5,6,7,4,8,9]
+
+x=[]
+for i in list_dup:
+    if i not in x:
+        x.append(i)
+    else:
+        continue
+print(x)
+
+#code to move all positive value to left side and -ve values to right side
+list_pn=[1,-2,3,-4,5,-6,7,-8,9]
+listp=[]
+listn=[]
+
+for i in list_pn:
+    if i>0:
+      listp.append(i)
+    else:
+        listn.append(i)
+a=listp+listn
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
