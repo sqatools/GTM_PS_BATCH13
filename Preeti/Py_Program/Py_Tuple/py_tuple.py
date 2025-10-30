28572e8a22dbc1418227b69ee49c218c1216afde
Preeti Gurav <guravpreeti2022@gmail.com>
2025-10-29 19:52:17 -0700

    tuple class work

--- Preeti/Py_Program/Py_Tuple/py_tuple.py
+++ Preeti/Py_Program/Py_Tuple/py_tuple.py
@@ -36,4 +36,36 @@ print("_" * 50)
 tup3 = (4, 7, 9, 3, 4, 8, 4)
 print(sum(tup3))
 
+# count method:
+tup3 = (4, 7, 9, 3, 4, 8, 4)
 
+print("count of 4 :", tup3.count(4))
+# count of 4 : 3
+
+# Index method:
+print("Index of 9 :", tup3.index(9))
+# Index of 9 : 2
+
+###################################################
+t1 = (5, 7, 9)
+t2 = ('a', 'b', 'c')
+
+t3 = t1 + t2
+print("t3 :", t3)  # (5, 7, 9, 'a', 'b', 'c')
+
+print("_"*50)
+##################################################
+# sorted function:
+
+tup4 = (5, 8, 12, 53, 7, 1, 2)
+# sorted data in ascending order
+result1 = sorted(tup4)
+print("Ascending order :", result1)  # [1, 2, 5, 7, 8, 12, 53]
+
+# sorted data in descending order
+result2 = sorted(tup4, reverse=True)
+print("descending order :", result2)  # [53, 12, 8, 7, 5, 2, 1]
+
+# reversed function:
+result3 = tuple(reversed(tup4))
+print("reversed output :", result3)  # (2, 1, 7, 53, 12, 8, 5)
