88873d3d41ed7d75001fac3c647581f3db860377
Shreya Satle <shreya11satle@gmail.com>
2025-10-31 08:23:32 +0530

    Merge remote-tracking branch 'origin/main'

--- Shreya/list_methods_2.py
+++ Shreya/list_methods_2.py
@@ -1,5 +1,5 @@
 ################################remove method############################
-list_1=[2,3,4,6,7,0]
+list_1=[2,3,4,6,7,0,-7,-55]
 list_1.remove(0)
 list_1.remove(4)
 print("list_1:", list_1)
@@ -8,3 +8,6 @@ print("list_1:", list_1)
 list_2= list_1.pop()
 print("removed value :", list_2)
 print("list_1 :",list_1)
+###########################sorting#################
+list_3= list(sorted(list_1, reverse=True))
+print("sorted result in decreasing :", list_3)
