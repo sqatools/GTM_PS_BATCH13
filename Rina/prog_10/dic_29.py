ad7ba1306806be6777d101f8d37c434c213bf046
rinars11 <rina.rinkesh@gmail.com>
2025-10-30 23:03:02 -0400

    clear method

--- Rina/prog_10/dic_29.py
+++ Rina/prog_10/dic_29.py
@@ -17,8 +17,16 @@ print(result)
 dict4 = {'e': (4,8,44), 'b': 'hello', 'a': 'pinch', 'g': (66,11,90)}
 val = dict4.pop('a')
 print("remove value: ", val)
-"""
+
 list1 = ['v', 'w', 'g']
 list2 = [10, 2200, 130]
 dict1 = dict(zip(list1, list2))
 print(dict1)
+
+"""
+
+dict7 = {'a': [3, 3, 3], 'b': [4, 4, 4], 'c': [9, 9, 9]}
+output = {}
+for k, v in dict7.items():
+    output[k] = sum(v)
+    print("output :", output)
