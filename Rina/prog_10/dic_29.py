45d5a5f4da71a275024995bc0947fec00c64edbd
rinars11 <rina.rinkesh@gmail.com>
2025-10-30 22:55:25 -0400

    clear method

--- Rina/prog_10/dic_29.py
+++ Rina/prog_10/dic_29.py
@@ -1,8 +1,8 @@
 """
-dic1 = {'a': 60, 'b': 94, 'c': 77}
-dic1['d'] = 100
-dic1['e'] = 1
-print("dic1: ", dic1)
+dict1 = {'a': 60, 'b': 94, 'c': 77}
+dict1['d'] = 100
+dict1['e'] = 1
+print("dic1: ", dict1)
 
 
 str1 = "We are learning Python"
@@ -14,4 +14,7 @@ for word in word_list:
     result[first_char] = word
 print(result)
 
+dict4 = {'e': (4,8,44), 'b': 'hello', 'a': 'pinch', 'g': (66,11,90)}
+val = dict4.pop('a')
+print("remove value: ", val)
 """
