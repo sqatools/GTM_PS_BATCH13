0c53902b13d8cc5b32599a7db742e9edabe45932
Preeti Gurav <guravpreeti2022@gmail.com>
2025-11-04 08:17:58 +0530

    python dictionary 4th nov class work

--- Preeti/Py_Program/Dictionary/dict_4_nov.py
+++ Preeti/Py_Program/Dictionary/dict_4_nov.py
@@ -19,3 +19,35 @@ for k, v in dict1.items():
 print("output :", output)
 # output : {'a': 6, 'b': 15, 'c': 24}
 
+
+############ get data from dictionary ####################
+from pprint import pprint
+school = {
+    'teacher': {
+        'English': [
+            {'name': 'Rohit', 'email': 'rohit@gmail.com', 'phone': 6546456},
+            {'name': 'Rahul', 'email': 'rahul@gmail.com', 'phone': 56432343}
+        ],
+        'Maths': [],
+    },
+    'student': {
+        '10th': [
+            {'name': 'mohit', 'email': 'mohit@gmail.com', 'phone': 8866868686},
+            {'name': 'akshay', 'email': 'akshay@gmail.com', 'phone': 876766766},
+        ],
+        '11th': [],
+    },
+    'admin': {
+        'account': [],
+        'admission': []
+    }
+
+}
+pprint(school)
+
+pprint(school['teacher'])
+pprint(school['teacher']['English'][0])
+pprint(school['teacher']['English'][1]['phone'])
+
+pprint(school['student'])
+pprint(school['student']['10th'][0]['phone'])
