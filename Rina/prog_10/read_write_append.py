1deb69df507773297b501a62eea97e3cafe42589
rinars11 <rina.rinkesh@gmail.com>
2025-11-06 21:55:52 -0500

    module

--- Rina/prog_10/read_write_append.py
+++ Rina/prog_10/read_write_append.py
@@ -1,3 +1,5 @@
+"""
+
 def read_file(file_path):
     readfile = open(file_path)
     data = readfile.read()
@@ -7,3 +9,15 @@ def read_file(file_path):
     print("File closed after: ", readfile.closed)
 
 read_file("read.txt")
+
+def read_file_with_context(filepath):
+    with open(filepath, 'r') as file_obj:
+        data = file_obj.read()
+        print(data)
+        print("File close before :", file_obj.closed)
+        print("File close after :", file_obj.closed)
+read_file_with_context("read.txt")
+
+"""
+
+
