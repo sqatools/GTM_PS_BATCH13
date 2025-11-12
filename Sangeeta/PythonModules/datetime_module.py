1f69a5529f8bb151906ea0703b5bd78d880dcdc9
sangeetasalian <sangeeta.salian@gmail.com>
2025-11-11 21:42:50 -0500

    pushing filehandling in git

---
+++ Sangeeta/PythonModules/datetime_module.py
@@ -0,0 +1,5 @@
+from datetime import datetime, timedelta
+
+current_date = datetime.now()
+print()
+
---
+++ Sangeeta/PythonModules/faker_module.py
@@ -0,0 +1,9 @@
+from faker import Faker
+
+fk = Faker()
+
+print ("first name :", fk.first_name())
+print ("last name :", fk.last_name())
+print ("email :", fk.email())
+print ("first name :", fk.first_name())
+
