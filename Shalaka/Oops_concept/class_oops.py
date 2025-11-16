02c2e54c0644494cf0ca7974fddaecd191bc0a7e
shabe2304 <shabel2304@gmail.com>
2025-11-16 22:53:45 +0530

    OPPS- Class & Constructor

--- Shalaka/Oops_concept/class_oops.py
+++ Shalaka/Oops_concept/class_oops.py
@@ -21,22 +21,3 @@ obj.my_car_price()
 
 
 
-class car_world:
-
-    def __init__(self):  #### constructor
-        print("Welcome to car world")
-
-        ### Calling method inside constructor=
-        self.car_price()
-
-
-        #### Method 1
-    def car_name(self):
-        print("My car is : TATA")
-
-        #### Method 2
-    def car_price(self):
-        print("10 lcas")
-
-obj1 = car_world()
-obj1.car_name()
