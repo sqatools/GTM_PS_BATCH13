00258f6f2e1f7043cc3f214e1ca99475c406fcac
rinars11 <rina.rinkesh@gmail.com>
2025-11-19 21:57:09 -0500

    decorator

--- Rina/prog_10/decorator.py
+++ Rina/prog_10/decorator.py
@@ -19,5 +19,22 @@ square(55)
 
 
 
+def value_cheker(func):
+    def inner (num):
+        if num > 20:
+            num = 20
+            func(num)
+        else:
+            func(num)
+
+    return inner
+
+@value_cheker
+def get_square_of_given_range(num):
+    for i in range(1, num):
+        print(i, ":", i**2)
+
+print("+"*50)
+get_square_of_given_range(30)
 
 
