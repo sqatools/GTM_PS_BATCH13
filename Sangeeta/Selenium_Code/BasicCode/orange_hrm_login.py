9c1d3ccba45da2d92b11820ae54e635a6c831ad3
sangeetasalian <sangeeta.salian@gmail.com>
2025-11-23 20:34:17 -0500

    OrangeHRM login script pushed to GIT

---
+++ Sangeeta/Selenium_Code/BasicCode/orange_hrm_login.py
@@ -0,0 +1,18 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+# title_header = driver.find_element(By.PARTIAL_LINK_TEXT,"Login")
+# print("Header text for the page is :", title_header)
+driver.find_element(By.NAME, "username").send_keys("Admin")
+driver.find_element(By.NAME, "password").send_keys("admin123")
+time.sleep(2)
+#driver.find_element(By.CSS_SELECTOR, ".oxd-button oxd-button--medium oxd-button--main orangehrm-login-button").click()
+driver.find_element(By.CLASS_NAME, "orangehrm-login-button").click()
+time.sleep(5)
+
+
