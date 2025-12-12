fc0030956c567615a962e4bc8d091f93d7d78909
maxpartha88 <max.partha@gmail.com>
2025-12-11 22:52:33 -0500

    Adding 8 files

---
+++ Partha/PyTest/AutomationFramework1/pageObjects/dummyWebsite/dummy_booking_page_locator.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+dob_cal = (By.ID, "birthday")
+female_radio_btn = (By.ID, "female")
+travellers_dropdown = (By.ID, "admorepass")
+
+from_city_field = (By.NAME, "fromcity")
+dest_city_field = (By.NAME, "destcity")
+billing_name_field = (By.ID, "billing_name")
+billing_phone_field = (By.ID, "billing_phone")
