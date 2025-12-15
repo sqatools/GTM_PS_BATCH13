4b38d7e082f249399382ae84b1259c01beefde2a
Sweetygit2025 <jadhavsweety67@gmail.com>
2025-12-15 08:38:37 +0530

    Adding waits and dropdown

---
+++ Sweety/AutomationFramework/base/selenium_base.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.select import Select
+import logging
+
+
+class SeleniumBase:
+    def __init__(self, driver, timeout=30):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, timeout=timeout)
+        self.log = logging.getLogger(__name__)
+
+    def get_element(self, locator):
+        self.log.info(f"getting element with locator: {locator}")
+        return self.wait.until(ec.presence_of_element_located(locator))
+
+    def click_element(self, locator):
+        self.log.info(f"clicking on element with locator: {locator}")
+        self.get_element(locator).click()
+
+    def enter_text(self, locator, value):
+        self.log.info(f"entering text: {value}, with locator: {locator}")
+        self.get_element(locator).send_keys(value)
+
+    def get_text(self, locator):
+        self.log.info(f"getting text with locator: {locator}")
+        return self.get_element(locator).text
+
+    def get_attribute_value(self, locator, attrib):
+        self.log.info(f"get attribute: {attrib}, value with locator: {locator}")
+        return self.get_element(locator).get_attribute(attrib)
+
+    def select_dropdown_value(self, locator, value):
+        self.log.info(f"selecting value: {value}, with locator: {locator}")
+        element = self.get_element(locator)
+        select = Select(element)
+        select.select_by_visible_text(value)
---
+++ Sweety/AutomationFramework/tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+from datetime import datetime
+from selenium import webdriver
+
+
+@pytest.fixture(scope='class')
+def get_driver(request):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    request.cls.driver = driver
+    yield
+    driver.close()
+
+
+def pytest_configure(config):
+    # create a logs folder if is not there
+    logs_path = os.path.join(os.getcwd(), "logs")
+    if not os.path.exists(logs_path):
+        os.mkdir(logs_path)
+
+    # create a folder with date name if it is not there
+    folder_name = datetime.now().strftime("%d_%m_%Y")
+    folder_path = os.path.join(logs_path, folder_name)
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+
+    # create a log file inside the today date folder.
+    unique_name = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    log_file_name = f"{unique_name}_trace.log"
+    log_file_path = os.path.join(folder_path, log_file_name)
+
+    # update log file path in config
+    config.option.log_file = log_file_path
