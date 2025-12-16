15896f34eac2761a1a4e286a0b9e9332b197c223
sqatools <sqatools.automation@gmail.com>
2025-12-16 08:07:02 +0530

    Framework read data from json file

---
+++ Deepesh/AutomationFramework/tests/login_test/test_login.py
@@ -0,0 +1,36 @@
+import time
+
+import pytest
+from ...page_objects.login_page.login_page_class import LoginPage
+from ...page_objects.login_page.login_page_data import json_file_path, jsn_data
+from ...utilities.utils_tools import Util
+
+@pytest.mark.usefixtures("get_driver")
+class TestLogin:
+
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self):
+        self.lp = LoginPage(self.driver)
+        self.util = Util()
+        self.json_data = self.util.read_json_data(json_file_path)
+        self.lp.launch_login_page(self.json_data["login_url"])
+
+
+    def test_verify_login_functionality(self):
+        self.lp.enter_username(self.json_data['username'])
+        time.sleep(3)
+        self.lp.enter_password(self.json_data['password'])
+        time.sleep(3)
+        self.lp.click_login_button()
+        time.sleep(3)
+
+
+    @pytest.mark.parametrize("user, passwd", jsn_data["users_list"])
+    def test_login_parametrization(self, user, passwd):
+        self.lp.launch_login_page(self.json_data["login_url"])
+        self.lp.enter_username(user)
+        time.sleep(1)
+        self.lp.enter_password(passwd)
+        time.sleep(1)
+        self.lp.click_login_button()
+
---
+++ Deepesh/AutomationFramework/utilities/utils_tools.py
@@ -0,0 +1,20 @@
+import json
+
+class Util:
+
+    def read_json_data(self, file_path):
+        with open(file_path, "r") as file:
+            data = file.read()
+            json_data = json.loads(data)
+        return json_data
+
+
+
+if __name__ == '__main__':
+    print(__name__)
+    obj = Util()
+    print(obj.__module__)
+    data = obj.read_json_data(file_path="users_data.json")
+    print(data)
+    print(data['email'])
+
