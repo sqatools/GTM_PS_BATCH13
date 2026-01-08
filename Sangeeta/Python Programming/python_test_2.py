8876f7fe80fc92e24b25c36749a8364663bf4261
sangeetasalian <sangeeta.salian@gmail.com>
2026-01-07 20:55:27 -0500

    framework config pushed to GIT

--- Sangeeta/Python Programming/python_test_2.py
+++ Sangeeta/Python Programming/python_test_2.py
@@ -7,22 +7,38 @@
 # # Bazooka
 # for char in str1:
 #     if char in list1:
-#         print(f'char ' + char + ' is a vowel ')
+#         print(f 'char ' + char + ' is a vowel ')
 #         count += 1
 #     else:
 #         print("This is not a vowel - ", char)
 # print("Total count of char is :", count)
 
-print("-*-"*50
-      )
+print("--*--"*50)
+
 print("List Operations – Even Numbers - Write a Python program that: \n Takes n integers as input from the user and stores them in a list.\n From this list, creates another list containing only the even numbers.\n")
 print("both the original list and the even-number list.")
 
 list1 = []
-list2= []
+list2 = []
 
 for num in range(10):
     num = int(input("Enter a number of your choice : ", ))
     list1.append(num)
-    print(list1)
+    print("list1 = ", list1)
+int_value = 0
+odd_no = []
+for val in list1:
+    val = val + int_value
+    if val % 2 == 0:
+        list2.append(val)
+    else:
+        odd_no.append(val)
+print("These numbers from List 2 are not even but are odd numbers", odd_no)
+print("List 2 numbers are even numbers = ", list2)
 
+print("**---**"*50)
+
+print("Write a python program- Takes names of student and marks for 3 subjects as input. \n Stores name in the form of dictionary {'name':<student_name>, 'marks':[m1,m2,m3]}")
+print("Calculates the average marks. \n Print dictionary and average")
+
+student1 = input()
