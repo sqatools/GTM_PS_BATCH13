8e6201f38e5dec3c17e2b48ece214c149ee9d632
rohitgit0006 <rohitchavan0006@gmail.com>
2026-01-13 23:27:53 +0530

    change in datafile

--- Rohit/automationexercise_Framework/tests/conftest.py
+++ Rohit/automationexercise_Framework/tests/conftest.py
@@ -2,16 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-def get_driver(request):
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    request.cls.driver = driver
-    yield
-    driver.quit()
-
-import pytest
-
+# Add CLI options
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name",
@@ -21,24 +12,29 @@ def pytest_addoption(parser):
     )
     parser.addoption(
         "--headless",
-        action="store_true",   # just a flag
+        action="store_true",
         default=False,
         help="Run browser in headless mode"
     )
 
+# Fixture to initialize driver
 @pytest.fixture(scope="class")
 def get_driver(request, pytestconfig):
-    browser_name = pytestconfig.getoption("browser_name")
+    browser_name = pytestconfig.getoption("browser_name").lower()
     headless = pytestconfig.getoption("headless")
 
-    if browser_name.lower() == "chrome":
+    if browser_name == "chrome":
         options = Options()
         if headless:
-            options.add_argument("--headless")
+            options.add_argument("--headless=new")  # safer for latest Chrome
+            options.add_argument("--disable-gpu")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options=options)
     else:
-        raise Exception(f"Browser {browser_name} not supported")
+        raise Exception(f"Browser '{browser_name}' is not supported!")
 
-    request.cls.driver = driver
-    yield
+    driver.maximize_window()
+    request.cls.driver = driver  # attach to the test class
+    yield driver  # yield driver instance to tests
     driver.quit()
