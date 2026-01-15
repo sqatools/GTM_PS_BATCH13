4ab8f14146b4c552d81901626dc4439fe446520a
rohitgit0006 <rohitchavan0006@gmail.com>
2026-01-15 21:38:02 +0530

    Pushing code to github

--- Rohit/automationexercise_Framework/tests/conftest.py
+++ Rohit/automationexercise_Framework/tests/conftest.py
@@ -5,14 +5,27 @@ from datetime import datetime
 
 
 @pytest.fixture(scope="class")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_driver(request):
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
+    request.cls.driver = driver
+    yield
+    driver.quit()
+
+'''
 def get_driver(request):
     driver = webdriver.Chrome()
     driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.quit()
-
-def pytest_addoption(parser):
-    parser.addoption("--browser_name", action="store")
-    parser.addoption("--headless", action="store_true")
+'''
 
