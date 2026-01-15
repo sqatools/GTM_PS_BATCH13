85032f68f1f5d3bfc41f9f3b7c744bc0e0c4ae87
rohitgit0006 <rohitchavan0006@gmail.com>
2026-01-15 21:53:29 +0530

    Pushing code to github

--- Rohit/automationexercise_Framework/tests/conftest.py
+++ Rohit/automationexercise_Framework/tests/conftest.py
@@ -1,27 +1,33 @@
 import pytest
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from ..base.Ecommerance import Ecommerce
 
 
-@pytest.fixture(scope="class")
-def get_driver(request):
-    options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-
-    driver = webdriver.Chrome(options=options)
-    request.cls.driver = driver
-    yield
-    driver.quit()
-
-'''
+@pytest.fixture(scope='class')
 def get_driver(request):
     driver = webdriver.Chrome()
     driver.maximize_window()
     request.cls.driver = driver
     yield
-    driver.quit()
-'''
+    driver.close()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action='store', default='chrome', help="browser to execute the automation")
+    parser.addoption("--headless", action='store', default=False, help="browser to execute the automation")
+
+
+@pytest.fixture(scope="class")
+def get_driver_with_headless(request, pytestconfig):
+    browser_name = pytestconfig.getoption("browser_name")
+    headless_value = pytestconfig.getoption("headless")
+    if headless_value == "True":
+        wf = Ecommerce(browser_name, headless_value)
+    else:
+        wf = Ecommerce(browser_name, headless=False)
+    driver = wf.get_driver_instance()
+    request.cls.driver = driver
+    yield
+    driver.close()
+
 
