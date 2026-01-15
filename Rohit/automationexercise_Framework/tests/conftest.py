5c18f2f56a32c3e0b8d1f44c9fb962914991f691
rohitgit0006 <rohitchavan0006@gmail.com>
2026-01-15 22:04:43 +0530

    change to Repository

--- Rohit/automationexercise_Framework/tests/conftest.py
+++ Rohit/automationexercise_Framework/tests/conftest.py
@@ -13,18 +13,15 @@ def get_driver(request):
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action='store', default='chrome', help="browser to execute the automation")
-    parser.addoption("--headless", action='store', default=False, help="browser to execute the automation")
+    parser.addoption("--browser_name", action='store', default='chrome', help="browser to execute the automation")
+    parser.addoption("--headless", action='store', default='False', help="run browser in headless mode")
 
 
 @pytest.fixture(scope="class")
 def get_driver_with_headless(request, pytestconfig):
     browser_name = pytestconfig.getoption("browser_name")
-    headless_value = pytestconfig.getoption("headless")
-    if headless_value == "True":
-        wf = Ecommerce(browser_name, headless_value)
-    else:
-        wf = Ecommerce(browser_name, headless=False)
+    headless_value = pytestconfig.getoption("headless").lower() == "true"
+    wf = Ecommerce(browser_name, headless=headless_value)
     driver = wf.get_driver_instance()
     request.cls.driver = driver
     yield
