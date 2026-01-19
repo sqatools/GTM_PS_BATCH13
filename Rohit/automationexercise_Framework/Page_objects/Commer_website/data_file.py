919dab291611a5d0d42dd80c9a1df02b3c845786
rohitgit0006 <rohitchavan0006@gmail.com>
2026-01-19 19:13:09 +0530

    made changes in locator and wait push to github

--- Rohit/automationexercise_Framework/Page_objects/Commer_website/data_file.py
+++ Rohit/automationexercise_Framework/Page_objects/Commer_website/data_file.py
@@ -2,7 +2,7 @@ website_url = "https://automationexercise.com/login"
 
 #New_User_Signup
 User_Name = "Rohit Chavan"
-User_Email_address = "rohit85@gmail.com"
+User_Email_address = "rohit86@gmail.com"
 
 #Enter Account Information:
 Password = "Karad@123"
@@ -24,7 +24,7 @@ Mobile_number = "5678906532"
 
 
 #Login to your account
-Login_Email_address= "rohit85@gmail.com"
+Login_Email_address= "rohit86@gmail.com"
 Login_Password = "Karad@123"
 
 #Category
