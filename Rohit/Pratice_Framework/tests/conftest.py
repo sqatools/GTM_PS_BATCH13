bf449c840936437fd09de07531a6e60fca023042
rohitgit0006 <rohitchavan0006@gmail.com>
2026-01-27 21:34:29 +0530

    add to repository

--- Rohit/Pratice_Framework/tests/conftest.py
+++ Rohit/Pratice_Framework/tests/conftest.py
@@ -8,3 +8,7 @@ def get_driver(request):
     request.cls.driver = driver
     yield
     driver.close()
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action='store', default='chrome', help="browser to execute the automation")
+    parser.addoption("--headless", action='store', default='False', help="run browser in headless mode")
