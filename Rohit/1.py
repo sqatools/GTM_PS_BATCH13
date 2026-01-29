96e5e2b8238f3c13e764c1b2d7b906ac5fb5b199
rohitgit0006 <rohitchavan0006@gmail.com>
2026-01-29 22:33:58 +0530

    Pushing files to github

--- Rohit/1.py
+++ Rohit/1.py
@@ -1,11 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import ActionChains
-
-
 list =[1,20,8,6,19]
 
 max_num = list[0]
