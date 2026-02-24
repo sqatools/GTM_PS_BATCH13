e0e16a94bf6e2027444e7e9249076274332df10c
rohitgit0006 <rohitchavan0006@gmail.com>
2026-02-24 21:16:46 +0530

    Pushing files to github

--- Rohit/mockbank_framework/Page/data_file.py
+++ Rohit/mockbank_framework/Page/data_file.py
@@ -1,8 +1,8 @@
 web_url = "https://practice.automationtesting.in/"
 
-Email_Address = "dheeraj.patil17@gmail.com"
+Email_Address = "dheeraj.patil18@gmail.com"
 Password_btn = "Samartha@1234567"
 
 #Login
-Login_Username = "dheeraj.patil17@gmail.com"
+Login_Username = "dheeraj.patil18@gmail.com"
 Login_Psd = "Samartha@1234567"
--- Rohit/mockbank_framework/base/selenium.py
+++ Rohit/mockbank_framework/base/selenium.py
@@ -8,11 +8,15 @@ class Base_Hitachi():
         self.driver = driver
         self.wait = WebDriverWait(driver,timeout)
 
-    def get_element(self,locator):
-        return self.wait.until(EC.element_to_be_clickable(locator))
+    def get_element(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
 
-    def click_element(self,locator):
-        self.get_element(locator).click()
+    def click_element(self, locator):
+        element = self.wait.until(EC.visibility_of_element_located(locator))
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block: 'center'});", element
+        )
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
 
     def enter_text(self,locator,text):
         self.get_element(locator).send_keys(text)
