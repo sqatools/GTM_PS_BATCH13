bb0a5b3fcbf28ae7bebee841fed7c0d690e4f440
rohitgit0006 <rohitchavan0006@gmail.com>
2026-03-05 19:52:43 +0530

    Add to Repository file

--- Rohit/mockbank_framework/Page/data_file.py
+++ Rohit/mockbank_framework/Page/data_file.py
@@ -1,10 +1,10 @@
 web_url = "https://practice.automationtesting.in/"
 
-Email_Address = "dheeraj.patil30@gmail.com"
+Email_Address = "dheeraj.patil31@gmail.com"
 Password_btn = "Samartha@1234567"
 
 #Login
-Login_Username = "dheeraj.patil30@gmail.com"
+Login_Username = "dheeraj.patil31@gmail.com"
 Login_Psd = "Samartha@1234567"
 
 # --- negative (invalid) credentials ---
