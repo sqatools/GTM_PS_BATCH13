994463683a3749ce212240b6bfcc66817c3ccfd7
rohitgit0006 <rohitchavan0006@gmail.com>
2026-03-06 18:32:37 +0530

    Add file to Repository

--- Rohit/mockbank_framework/test/conftest.py
+++ Rohit/mockbank_framework/test/conftest.py
@@ -1,10 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope="class")
 def get_driver(request):
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
+
     request.cls.driver = driver
-    yield
-    driver.close()
+    yield driver
+    driver.quit()
