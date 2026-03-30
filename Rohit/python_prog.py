4dbe5bfdfdcc98078defeebc19fa94d8e1b3679c
rohitgit0006 <rohitchavan0006@gmail.com>
2026-03-30 15:07:11 +0530

    Pushing files to github1

--- Rohit/python_prog.py
+++ Rohit/python_prog.py
@@ -1,11 +1,18 @@
 rows = 4
-
 for i in range(1, rows + 1):
     # Print spaces
     print(" " * (rows - i), end="")
-
     # Print numbers
     for j in range(1, i + 1):
         print(j, end="   ")
-
     print()
+
+s = "automation"
+vowels = "aeiou"
+count = 0
+
+for ch in s:
+    if ch in vowels:
+        count += 1
+
+print(count)
