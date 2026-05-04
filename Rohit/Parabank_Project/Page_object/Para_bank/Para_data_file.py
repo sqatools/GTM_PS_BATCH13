e892c9f3c0d9ebf5fee7defed24d81b18b38409e
rohitgit0006 <rohitchavan0006@gmail.com>
2026-05-04 15:51:29 +0530

    Add files to Repositpory

--- Rohit/Parabank_Project/Page_object/Para_bank/Para_data_file.py
+++ Rohit/Parabank_Project/Page_object/Para_bank/Para_data_file.py
@@ -10,10 +10,10 @@ state = "Maharastra"
 zipcode = "415119"
 phone = "9876543212"
 ssn = "890763"
-username = "manalichavan900@yahoo.com"
+username = "shirdharth_mohite8980@gmail.com"
 password = "Rohit@123"
 confirm_password = "Rohit@123"
 
 #Customer Login
-User_name = "manalichavan900@yahoo.com"
+User_name = "shirdharth_mohite8980@gmail.com"
 Password = "Rohit@123"
